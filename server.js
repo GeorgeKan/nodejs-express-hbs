6a445b9f01ddf9544c2cfa33bb29a884c94870c8
Sophia Katsaiti <Sophia Katsaiti>
2017-02-17 21:36:20 +0200

    'Arxiko'

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+var now = new Date().toString();
+var log = `${now} : ${req.method} : ${req.url}`;
+
+fs.appendFile('server.log' , log + '\n', (error) => {
+    if(error){
+        console.log(error);
+    }
+});
+console.log(log);
+next();
+});
+
+app.use((req, res, next) => {
+
+res.render('maintenance.hbs');
+
+});
+
+app.set('view enine', 'hbs');
+hbs.registerHelper('getcopyrightYear', () => {
+return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamit', (test) => {
+return test.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+
+res.render('home.hbs', {
+    welcomeMessage : 'Welcome to my Home page',
+    pageTitle : 'Home Page of Gkan'
+});
+
+// res.send('<h2>Hello Gkan Express</h2>');
+// res.send({
+//     name : 'George',
+//     love : 'Sofia',
+//     all : 'george + sofia',
+//     like : [
+//         'Eating',
+//         'Walking',
+//         'traveling'
+//     ]
+// });
+});
+
+app.get('/about', (req, res) => {
+// res.send('<b>About Page Gkan</b>');
+res.render('about.hbs', {
+pageTitle : 'About Gkans Page'
+});
+});
+
+app.get('/bad', (req, res) => {
+res.send({
+    errorMessage : 'Bad Request'
+});
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
