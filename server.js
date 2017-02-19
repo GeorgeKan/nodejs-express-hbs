efce5fadca433de0c68470ec99bcb6388bc46db3
Sophia Katsaiti <Sophia Katsaiti>
2017-02-19 20:31:15 +0200

    Setup startup script and herocu port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -20,11 +22,9 @@ console.log(log);
 next();
 });
 
-app.use((req, res, next) => {
-
-res.render('maintenance.hbs');
-
-});
+// app.use((req, res, next) => {
+// res.render('maintenance.hbs');
+// });
 
 app.set('view enine', 'hbs');
 hbs.registerHelper('getcopyrightYear', () => {
@@ -69,6 +69,6 @@ res.send({
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log('Server is up on port : ' + port);
 });
